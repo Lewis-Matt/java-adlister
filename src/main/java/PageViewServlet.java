82b14c660dc83ff322fa96dbd737b5b8afdb4b2e
Matthew Lewis <matthew.d.lewis64@gmail.com>
2022-03-12 14:32:32 -0600

    added servlet

---
+++ src/main/java/PageViewServlet.java
@@ -0,0 +1,7 @@
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+@WebServlet(name = "PageViewServlet", urlPatterns = "/count")
+public class PageViewServlet extends HttpServlet {
+
+}
