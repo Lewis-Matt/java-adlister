642f7b1e1c91eb532e8526a8611589cdaf25176c
Matthew Lewis <matthew.d.lewis64@gmail.com>
2022-03-12 15:24:08 -0600

    tested and working servlets, added comments

--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -5,9 +5,9 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-
 // TODO: Create a page that displays "Hello World!" when a user visits /hello
 //  Make the page say "Hello, <name>!" if name is passed as part of the query string (e.g. /hello?name=codeup), otherwise default to "Hello, World!".
+
 // WebServlet annotation. This allows us to specify what URL this servlet should handle. http://localhost:8080/hello
 @WebServlet(name = "HelloWorldServlet", urlPatterns = "/hello")
 public class HelloWorldServlet extends HttpServlet {
@@ -18,15 +18,20 @@ public class HelloWorldServlet extends HttpServlet {
         res.setContentType("text/html");
         // Get a reference to a PrintWriter object (and add IOException to signature)
         PrintWriter out = res.getWriter();
-        // Store search query
-        // Even though  servlet can receive parameters in a number of different ways, every servlet retrieves its parameters the same way, using getParameter() and getParameterValues()
+        // Even though  servlet can receive parameters in a number of different ways, every servlet retrieves its parameters the same way, using getParameter() and getParameterValues() - see notes in codeup-java-exercises
         // getParameter() returns the value of the named parameter as a String or null if the parameter was not specified.
         String query = req.getParameter("name");
-        // Use that object to send some HTML as a response.
+        // Use the 'out' object to send HTML as a response.
         if (query == null) {
+            // Default
             out.println("<h1>Hello, World!</h1>");
         } else {
+            // Passed in username
             out.printf("<h1>Hello, %s!</h1>%n", query);
         }
     }
 }
+// Run the server. It will open browser to http://localhost:8080, which is a 404 because I didn't create a main page
+// USE:
+// http://localhost:8080/hello?name=Matt  OR
+// http://localhost:8080/count
--- src/main/java/PageViewServlet.java
+++ src/main/java/PageViewServlet.java
@@ -1,7 +1,26 @@
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+// TODO: Create a page that displays a number that goes up by one every time the /count page is viewed.
+//  Allow the user to pass a parameter in the query string to reset the counter.
 
 @WebServlet(name = "PageViewServlet", urlPatterns = "/count")
 public class PageViewServlet extends HttpServlet {
+    private Integer viewCount = 0;
 
+    protected void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
+        // .getParameter(Sting) the string can be anything, usually 'q' if it is a search query.
+        String query =req.getParameter("count");
+        viewCount++;
+        // If the user doesn't enter anything
+        if(query == null) {
+            res.getWriter().write("Total Page Views: " + viewCount);
+        } else {
+            viewCount = Integer.valueOf(query);
+            res.getWriter().write("Page Views Reset To: " + viewCount);
+        }
+    }
 }
