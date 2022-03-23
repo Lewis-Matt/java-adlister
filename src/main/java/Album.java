244df5417f548017b1e9b7ec2721aa67ddf43b76
Matthew Lewis <matthew.d.lewis64@gmail.com>
2022-03-23 09:49:14 -0500

    implemented album

---
+++ src/main/java/Album.java
@@ -0,0 +1,15 @@
+import java.io.Serializable;
+
+public class Album implements Serializable {
+
+
+}
+/*
+* ALBUMS MySQL Table:
+    id — auto-incrementing unsigned integer primary key
+    artist — string for storing the recording artist name
+    name — string for storing a record name
+    release_date — integer representing year record was released
+    sales — floating point value for number of records sold (in millions)
+    genre — string for storing the record's genre(s)
+*/
